be681681bec8e0051e0186dcb8589f89ad968791
brewern5 <natebrewer092001@gmail.com>
2025-05-07 00:29:33 -0400

    fixed logout issue - Signup issue persists
    Started work on issues
    - Sign Up does not recognize a users password using special characters
    
    - fixe logout issue

--- webroot/server/public/assets/js/auth.js
+++ webroot/server/public/assets/js/auth.js
@@ -1,39 +1,9 @@
 import("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js");
 
-function checkLoginStatus(){
-    if(sessionStorage.length > 0){
-        document.getElementById('user-state').innerHTML = 
-        `<a id="log-out" title="Log Out" href="">Log Out</a>`;
-
-        document.getElementById('my-account').innerHTML =
-        `<a class="top-link-myaccount" title="My Account" href="dashboard.html">My Account</a>`;
-
-        document.getElementById('log-out').addEventListener('click', function() {
-
-            logout(sessionStorage.getItem('token')).then(function (response){
-                if(response === true){
-                    sessionStorage.clear();
-                    localStorage.clear();
-                    window.replace('./detail.html');
-                }
-            });
-        });
-    }
-    else{
-        document.getElementById('user-state').innerHTML = 
-        `<a id="log-in" title="Log In" href="login.html">Login</a>`;
-        document.getElementById('my-account').innerHTML =
-        ``;
-
-    }
-}
 
 async function createNewUser(username, password, email, number, birthday) {
     try{
         const response = await axios.post('http://localhost:3000/api/users/signup', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
             content:{
                 username: username,
                 password: password,
@@ -41,13 +11,12 @@ async function createNewUser(username, password, email, number, birthday) {
                 number: number,
                 birthday: birthday
             }
-        }).then( function (response){
+        }).then(function (response){
             console.log(response);
             window.location.replace('./login.html');
         });
     } catch (error){
         console.log(error);
-        console.log(response);
     }
 }
 
@@ -104,7 +73,6 @@ async function authenticate(email, password) {
                 return response.data;
             }
         });
-        console.log(response);
         return response;
 
     }
@@ -144,21 +112,25 @@ async function reauthenticate(){
 
         });
     } catch(error){
-        console.log(resposne)
+        console.log(error);
     }
 }
 
 async function logout(token) {
     try{
-        const response = axios.post('http://localhost:3000/api/users/signout', {},{
+        const response = await axios.post('http://localhost:3000/api/users/signout', {},{
             headers: {
                 'Authorization': 'Bearer ' + token
             }
         }).then(function (response){
-            if(response.status == 200){
+            if(response.status === 200){
                 return true;
             }
         });
+
+        if(response === true){
+            return true;
+        }
     } catch(error){
         alert(error);
     }
